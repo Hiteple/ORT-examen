307477fc071e03e7917df7305c1f7bfaf268da2d
hiteple <hiteplegold@gmail.com>
2021-09-22 22:14:01 -0300

    Fix en if

--- search.js
+++ search.js
@@ -7,14 +7,16 @@ const pathFiles = path.join(__dirname, './files');
 
 const text = process.argv[2];
 
-fs.readdir(pathFiles, (err, data) => {
-    data.forEach((fileName) => {
-        fs.readFile(pathFiles + '/' + fileName, 'utf8', async (err, data) => {
-            const fileData = await data;
-
-            if (fileData.toString().toUpperCase().includes(text.toUpperCase())) {
-                console.log(fileName);
-            }
+if (text) {
+    fs.readdir(pathFiles, (err, data) => {
+        data.forEach((fileName) => {
+            fs.readFile(pathFiles + '/' + fileName, 'utf8', async (err, data) => {
+                const fileData = await data;
+    
+                if (fileData.toString().toUpperCase().includes(text.toUpperCase())) {
+                    console.log(fileName);
+                }
+            });
         });
     });
-});
+}
